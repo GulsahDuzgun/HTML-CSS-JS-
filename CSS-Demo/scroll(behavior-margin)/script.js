8e4ce3f59b597c1cbc1dbb37897894ce30f39966
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-06-17 13:07:37 +0300

    a.link add or remove active class with hashchange

---
+++ CSS-Demo/scroll(behavior-margin)/script.js
@@ -0,0 +1,17 @@
+const getHash = () => window.location.hash || '#about'
+
+const hashActive = () => {
+    [...document.querySelectorAll("a.link")]
+    .map(link => {
+        link.classList.remove('active')
+        return link
+    })
+    .find(link => link.hash === getHash())
+    .classList.add('active')
+
+}
+
+
+window.addEventListener("hashchange", hashActive);
+
+hashActive()
